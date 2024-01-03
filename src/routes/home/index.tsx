bf319b19c9f3b0dda1b992994f38ccef8f817b54
Dogtiti <499960698@qq.com>
2024-01-03 20:49:43 +0800

    fix: build error

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,6 +1,5 @@
 import useDarkMode from 'use-dark-mode';
 const Home = () => {
-	const darkMode = useDarkMode(false);
 	return <h1 className='text-black dark:text-white text-center'>GitMaya</h1>;
 };
 
