483bc0aa4923748fa03134ca40785f7731b75ee2
DDMeaqua <lzlloveyou@vip.qq.com>
2024-02-19 14:34:17 +0800

    chore: 使用b站提供的嵌入代码

--- src/routes/home/components/videoPlayer.tsx
+++ src/routes/home/components/videoPlayer.tsx
@@ -27,17 +27,21 @@ const VideoPlayer = ({ onClose }) => {
       <div
         style={{
           width: '100vw',
+          height: '56.25vw',
           maxWidth: '1280px',
           maxHeight: '720px',
           boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
         }}
       >
-        <video
-          style={{ width: '100%', height: '100%', borderRadius: '20px' }}
-          controls
-          autoPlay
-          src="./videoMaya.mp4"
-        />
+        <iframe
+          src="//player.bilibili.com/player.html?aid=241596420&bvid=BV1se411n7tD&cid=1413412972&p=1"
+          style={{ width: '100%', height: '100%' }}
+          scrolling="no"
+          border="0"
+          frameBorder="no"
+          framespacing="0"
+          allowFullScreen="true"
+        ></iframe>
       </div>
     </div>,
     document.body,
